3343742b40ce49992e909ad4873b480f7f5149f1
zardam <zardamhussain@gmail.com>
2024-04-19 01:17:31 +0530

    send-token-created

---
+++ app/api/send-otp/route.js
@@ -0,0 +1,36 @@
+import { supabase } from "@/lib/utils";
+import { Resend } from 'resend';
+import { EmailTemplate } from '@/components/email-template';
+
+import * as jwt from "jsonwebtoken";
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req) {
+    const { email } = await req.json();
+
+    if(!email) 
+        return new Response(JSON.stringify({msg : 'Please provide a email'}, { status: 401 }));
+
+    
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    const token = jwt.sign({ email, otp }, process.env.JWT_KEY, { expiresIn: '1h' });
+
+    await supabase.rpc('insert_into_otp_table', { email:email, otp: otp });
+    
+    try {
+      const data = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: email,
+        subject: 'Construction Key',
+        react: EmailTemplate({ name: otp }),
+      });
+  
+      return Response.json(JSON.stringify({token}));
+    } catch (error) {
+      return Response.json({ error });
+    }
+}
+
---
+++ components/email-template.jsx
@@ -0,0 +1,5 @@
+export const EmailTemplate = ({ name }) => (
+  <div>
+    <h1>Welcome, {name}!</h1>
+  </div>
+);
