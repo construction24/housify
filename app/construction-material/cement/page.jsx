0d9d1626180859e55ddd3c5ebf4ac73f5da72632
archit669 <architjain669@gmail.com>
2024-04-26 03:07:18 +0530

    update banner cement

--- app/construction-material/cement/page.jsx
+++ app/construction-material/cement/page.jsx
@@ -8,7 +8,7 @@ export default function page() {
   return (
     <div>
         <CategoryLayout
-            imagePath = "/construction-material-pages/category_banner_cement.webp"        
+            imagePath = "construction-material-pages/category_banner_Cement.png"        
         />
     </div>
   )
